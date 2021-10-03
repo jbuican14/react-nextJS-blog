e9f15815fad64b0ab28bad4b966800c80dabdce9
Juti Buican <jbuican19@gmail.com>
2021-10-03 12:39:50 +0100

    Add connected form client side to our api

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,24 +1,64 @@
+import {useState} from "react";
 import classes from "./contact-form.module.css";
 
 function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    // option for validate data client side
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(evt) => setEnteredEmail(evt.target.value)}
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your name</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(evt) => setEnteredName(evt.target.value)}
+            />
           </div>
         </div>
 
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea name="" id="message" rows="5"></textarea>
+          <textarea
+            required
+            name=""
+            id="message"
+            rows="5"
+            value={enteredMessage}
+            onChange={(evt) => setEnteredMessage(evt.target.value)}
+          ></textarea>
         </div>
 
         <div className={classes.actions}>
---
+++ pages/api/contact.js
@@ -0,0 +1,31 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const {email, name, message} = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({message: "Invaid input."});
+      return;
+    }
+
+    // store in db
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    console.log(newMessage);
+    res
+      .status(201)
+      .json({message: "Successfuly stored message", message: newMessage});
+  }
+}
+
+export default handler;
